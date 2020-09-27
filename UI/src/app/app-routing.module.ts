132929561fa7081409cc62e1ca16103cc8ffb8ca
manu <manukarnikas97@gmail.com>
2020-09-27 14:06:00 +0530

    config

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../app/Authguard/authguard.service';
+import { ConfigurationComponent } from '../app/configuration/configuration.component';
+import { BookConfigurationComponent } from '../app/configuration/book/book.component';
+import { LoginComponent } from '../app/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [AuthGuard],
+  },
+
+  {
+    path: 'configuration',
+    component: ConfigurationComponent,
+    // canActivate: [AuthGuard],
+
+    children: [
+      { path: 'books', component: BookConfigurationComponent },
+      { path: 'user', component: BookConfigurationComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
