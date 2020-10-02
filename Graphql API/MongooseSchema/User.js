15c00ad21c7d950da15348e330328e9fade00e56
manu <manukarnikas97@gmail.com>
2020-10-02 19:03:07 +0530

    Graphql Api

---
+++ Graphql API/MongooseSchema/User.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var userSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    mobileNumber: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        required: true
+    },
+
+}, {
+    timestamps: true
+});
+
+
+var user = mongoose.model('User', userSchema);
+
+module.exports = user;
